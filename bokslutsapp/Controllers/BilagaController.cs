b941d1bcaa70698754db0afd1bf812b6f9b5a251
davidhegardt <David.hegardt@gmail.com>
2018-04-22 15:37:22 +0200

    Test add data

--- bokslutsapp/Controllers/BilagaController.cs
+++ bokslutsapp/Controllers/BilagaController.cs
@@ -18,20 +18,40 @@ namespace Bokslutsapp.Controllers
 
         public ActionResult Bank()
         {
-            var Bilagor = GetBilagor();
+            var Bilagor = GetBilagor();            
             return View(Bilagor);
         }
 
         private IEnumerable<_1930Bank> GetBilagor()
         {
-            SieManager manager = new SieManager();            
+            SieManager manager = new SieManager();
+            var root = AppDomain.CurrentDomain.BaseDirectory;
+            string path = root + "/Assets/test.se";
+            List<Konto> kontoLista = manager.getKontoList(path);
+            int currId = 0;
+            List<_1930Bank> bankLista = new List<_1930Bank>();
+            foreach(Konto k in kontoLista)
+            {
+                currId++;
+                _1930Bank newBank = new _1930Bank();
+                newBank.Konto = k.Kontonummer;
+                newBank.Beskrivning = k.Kontonamn;
+                newBank.Belopp = 1237374.58f;
+                newBank.Id = currId;
+                newBank.Ks = 1;
+                newBank.Pr = 1;
+                bankLista.Add(newBank);
+            }
 
+            return bankLista;
+            /*
             return new List<_1930Bank> {
             new _1930Bank { Id = 1, Beskrivning = "Affärskonto SHB-590123123", Konto = 1930, Ks = 1, Pr = 1, Belopp = 127374.58f },
             new _1930Bank { Id = 2, Beskrivning = "Sparkonto SHB-590456123", Konto = 1931, Ks = 1, Pr = 1, Belopp = 567374.20f },
             new _1930Bank { Id = 3, Beskrivning = "Sparkonto SHB-59789456", Konto = 1932, Ks = 1, Pr = 1, Belopp = 7374.78f },
             new _1930Bank { Id = 4, Beskrivning = "Placeringskonto Avanza", Konto = 1933, Ks = 1, Pr = 1, Belopp = 84380.23f }
             };
+            */
         }
 
         public ActionResult Skatt()
--- bokslutsapp/Controllers/HomeController.cs
+++ bokslutsapp/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SieParserLibrary;
+using Bokslutsapp.Models;
 
 namespace Bokslutsapp.Controllers
 {
@@ -29,7 +31,41 @@ namespace Bokslutsapp.Controllers
 
         public ActionResult Kontologik()
         {
-            return View();
+            var Bilagor = GetBilagor();
+            return View(Bilagor);
+        }
+
+        private IEnumerable<_1930Bank> GetBilagor()
+        {
+            SieManager manager = new SieManager();
+            var root = AppDomain.CurrentDomain.BaseDirectory;
+            string path = root + "/Assets/test.se";
+            List<Konto> kontoLista = manager.getKontoList(path);
+            int currId = 0;
+            List<_1930Bank> bankLista = new List<_1930Bank>();
+            foreach (Konto k in kontoLista)
+            {
+                currId++;
+                _1930Bank newBank = new _1930Bank();
+                newBank.Konto = k.Kontonummer;
+                newBank.Beskrivning = k.Kontonamn;
+                newBank.Belopp = 1237374.58f;
+                newBank.Id = currId;
+                newBank.Ks = 1;
+                newBank.Pr = 1;
+                bankLista.Add(newBank);
+                //bankLista.Where(p => p.Beskrivning.Contains("Konto") && p.Belopp != 0); EXEMPEL PÅ LINQ sökning / filtrering
+            }
+
+            return bankLista;
+            /*
+            return new List<_1930Bank> {
+            new _1930Bank { Id = 1, Beskrivning = "Affärskonto SHB-590123123", Konto = 1930, Ks = 1, Pr = 1, Belopp = 127374.58f },
+            new _1930Bank { Id = 2, Beskrivning = "Sparkonto SHB-590456123", Konto = 1931, Ks = 1, Pr = 1, Belopp = 567374.20f },
+            new _1930Bank { Id = 3, Beskrivning = "Sparkonto SHB-59789456", Konto = 1932, Ks = 1, Pr = 1, Belopp = 7374.78f },
+            new _1930Bank { Id = 4, Beskrivning = "Placeringskonto Avanza", Konto = 1933, Ks = 1, Pr = 1, Belopp = 84380.23f }
+            };
+            */
         }
     }
 }
